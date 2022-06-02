8a8ff567db0840cc10ad5f446a583dd488474342
Linh Vu <linh777gkog@gmail.com>
2022-06-02 07:38:26 +0700

    toan tu so hoc

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-/**
- *
- * Giới thiệu về toán tử trong Javascript
- * 1. Toán tử số học - Arithmeic
- * 2. Toán tử gán - Assignment
- * 3. Toán tử so sánh - Comparison
- * 4. Toán tử logic - Logical
- *
- *
+/** Toán tử số học
+ +    --> Cộng
+ -    --> Trừ
+ *    --> Chia
+ **   --> Lũy thừa
+ /    --> Chia
+ %    --> Chia lấy số dư
+ ++   --> Tăng 1 giá trị số
+ --   --> Giảm 1 giá trị số
  */
 
 var a = 1;
 var b = 2;
 
-if (a > 0 && b > 0) {
-  // code ne
-  alert("a & b lon hon 0");
-}
+var c = a % b;
+
+console.log(c);
