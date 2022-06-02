b10dcc3ca2687e53d269e8dd84a33ef2eb215394
Linh Vu <linh777gkog@gmail.com>
2022-06-02 22:12:29 +0700

    toan tu gan

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-/** Toán tử số học
- +    --> Cộng
- -    --> Trừ
- *    --> Chia
- **   --> Lũy thừa
- /    --> Chia
- %    --> Chia lấy số dư
- ++   --> Tăng 1 giá trị số
- --   --> Giảm 1 giá trị số
+/**
+ *
+ * Giới thiệu về toán tử trong Javascript
+ * 1. Toán tử số học - Arithmeic
+ * 2. Toán tử gán - Assignment
+ * 3. Toán tử so sánh - Comparison
+ * 4. Toán tử logic - Logical
+ *
+ *
  */
 
 var a = 1;
-var b = 2;
 
-var c = a % b;
+a = a + 2;
 
-console.log(c);
+console.log(a);
