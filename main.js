82a55f68eef5e9d4ddc6f230415ea17b4ffd7fe2
Linh Vu <linh777gkog@gmail.com>
2022-06-04 22:40:06 +0700

    function

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-// Hiểu hơn về câu lệnh điều kiện
-// và phép so sánh
-// 0 , '', null, undefined, NaN, false => convert sang boolean
-// se hieu la false, con lai la true
-//
-var a = 1;
-var b = 2;
+/**
+ * Hàm  (function) trong JavaScript
+ * 1. Hàm?
+ *  - một khối mã
+ *  - Làm một việc cụ thể
+ * 2. Loại hàm
+ *  - built-in
+ *  - Tự định nghĩa
+ * 3. Tính chất
+ *  - Không thực thi khi định nghĩa
+ *  - Sẽ thực thi khi được gọi
+ *  - Có thể nhận tham số
+ *  - Có thể trả về một 1 giá trị
+ * 4. Tạo hàm đầu tiên
+ */
+// a-z A-Z 0-9 _ $
+// khong dat so lam ky tu dau tien
+function showDialog() {
+  alert("Hi");
+}
 
-// su dung toan tu & dong ve trai qua ve phai,
-// gap gia tri khac 6 gia tri tren => doc sang ve phai
-// lay gia tri ve phai gan nguoc lai result
-var result = "A" || "B" || "C" || "D";
-// Or thấy khác 6 value => lấy luôn
-
-console.log("result", result);
-// if nhan gia trij la true
-// if (result) {
-//   console.log("Dieu kien dung");
-// } else {
-//   console.log("Dieu kien sai");
-// }
+// call ()
+showDialog();
