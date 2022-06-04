dc53abf7d14d35091df3afea8b38d572b8a2b4ce
Linh Vu <linh777gkog@gmail.com>
2022-06-04 20:14:44 +0700

    toan tu so sanh

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 /**
- * Toán tử chuỗi - String operator
+ * Toan tu so sanh
+ *
+ *
+ * toan tu
+ * == => bang
+ * != => ko bang
+ * > lon hon
+ * < nho hon
+ * >= lon hon hoac bang
+ * <= nho hon hoac bang
+ *
  */
-var name = "Son";
-name += "  Dang";
 
-console.log(name);
+var a = 1;
+var b = 2;
+if (a < b) {
+  console.log("dieu kien dung!");
+} else {
+  console.log("dieu kien sai!");
+}
