2246bd9b98b941fc370ef3eb2fca0aef7a07dbc0
Linh Vu <linh777gkog@gmail.com>
2022-06-04 21:57:53 +0700

    documen.all is ngoaile

--- main.js
+++ main.js
@@ -7,9 +7,20 @@ Trong javascript co 6 gia tri sau duoc coi la Falsy
 5. undefined
 6. NaN*/
 
-console.log(!!false);
-console.log(!!0);
-console.log(!!"");
-console.log(!!null);
-console.log(!!undefined);
-console.log(!!NaN);
+/**
+ * Ngoai 6 gia  tri da de cap o phan falsy thi toan bo cac gia tri khác đều là Truthy , kể cả những giá trị sau:
+ * 1. '0'
+ * 2. ' '
+ * 3. 'false'
+ * 4. []
+ * 5. {}
+ * 6. function( ) {}
+ */
+
+/**
+ * DOCUMENT.ALL la mot ngoai le duy nhat  theo đặc tả ECMA (phiên bản 5). Đặc tả này mô tả toàn bộ các object khi chuyển sang boolean sẽ là true. Tuy nhiên, document.all là một ngoại lệ
+ * Cụ thể như sau:
+ * 1. Document.all chuyển sang boolean sẽ là false
+ * 2. document.all khi là toán hạng của toán tử so sanh == hoặc !== sẽ là undefined
+ * 3. Khi typeof document.all sẽ trả về "undefined"
+ */
