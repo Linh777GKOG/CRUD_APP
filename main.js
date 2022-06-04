4f9273261b76d8764ec618735b6e1ac40238c6a9
Linh Vu <linh777gkog@gmail.com>
2022-06-04 20:21:35 +0700

    kieu du lieu boolean

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 /**
- * Toan tu so sanh
- *
- *
- * toan tu
- * == => bang
- * != => ko bang
- * > lon hon
- * < nho hon
- * >= lon hon hoac bang
- * <= nho hon hoac bang
- *
+ * Boolean
  */
+// boolean chi the hien gia tri dung hay sai thoi
 
 var a = 1;
 var b = 2;
-if (a < b) {
-  console.log("dieu kien dung!");
-} else {
-  console.log("dieu kien sai!");
-}
+
+var isSuccess = a > b;
+console.log(isSuccess);
