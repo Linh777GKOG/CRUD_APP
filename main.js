b67008ec96f885aa318176a632b8320ac32023d9
Linh Vu <linh777gkog@gmail.com>
2022-06-04 19:53:57 +0700

    toan tu chuoi

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
 /**
- *
- * Giới thiệu về toán tử trong Javascript
- * 1. Toán tử số học - Arithmeic
- * 2. Toán tử gán - Assignment
- * 3. Toán tử so sánh - Comparison
- * 4. Toán tử logic - Logical
- *
- *
+ * Toán tử chuỗi - String operator
  */
+var name = "Son";
+name += "  Dang";
 
-// Toán tử ++ giúp tăng giá trị của một biến mang giá trị số lên 1.
-// Prefix & Postfix
-
-var number = 6;
-var output = ++number * 2 - number-- * 2;
-
-console.log("output: ", output);
+console.log(name);
