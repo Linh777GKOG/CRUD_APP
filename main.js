8b7b002e205302a047da35366c24d5faf8831142
Linh Vu <linh777gkog@gmail.com>
2022-06-04 23:05:55 +0700

    Dinh nghia nhieu tham sO

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 /* 
 1. Tham so?
 - Dinh nghia: Một giá trị có thể truyền vào khi gọi 1 function
-- Kieu du lieu?
-- Tinh private?
-- 1 tham so
+- Kieu du lieu: Ko Gioi han
+- Tinh private: chỉ chạy trong hàm
+- 1 tham so: 
 - Nhieu tham so
 2. Truyen tham so
 - 1 tham so
@@ -16,10 +16,11 @@
 - Gioi thieu vong for
 */
 
-function writeLog(message) {
+function writeLog(message, message2) {
   console.log(message);
+  console.log(message2);
   // Dinh nghia, xuat hien, su dung trong ham goi la tham so
 }
 
-writeLog("Test message");
+writeLog("TEST", "TEST_2");
 // Goi toi function, truyen gia tri vao => Đối số
