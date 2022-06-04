7ac50e802c21fb2a08cc85a6df36ab3b93737e6e
Linh Vu <linh777gkog@gmail.com>
2022-06-04 21:48:33 +0700

    Falsy

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-console.log(!!1);
-// true
-console.log(!!"f8");
-console.log(!!["Mercedes"]);
-// them !! phia truoc cac gia tri truthy se luon tra ve true
+/* 
+Trong javascript co 6 gia tri sau duoc coi la Falsy
+1. false
+2. 0
+3. '' or ""( chuoi rong)
+4. null
+5. undefined
+6. NaN*/
+
+console.log(!!false);
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!NaN);
