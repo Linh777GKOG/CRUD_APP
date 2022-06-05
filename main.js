0da25fb9fc69e974e1fc26f6744df4b03e223775
Linh Vu <linh777gkog@gmail.com>
2022-06-05 23:30:47 +0700

    create array

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
 /* 
-  Kieu so (Number) trong JavaScript
-  
-  1. Tao gia tri Number
-  - Cac cach tao
-  - Dung cach nao? Tai sao?
-  - Kiem tra data type
+Mảng trong JavaScript - Array
 
-  2. Lam viec voi Number
-  - To string
-  - To Fixed
-  */
-var age = 18;
-var PI = 3000.1235512343;
-console.log(PI.toFixed(2));
-
-// var otherNumber = new Number(9);
-// tranh cach dung new => Vì nó sẽ khởi tạo đối tượng(OBject)
-// đem lại hệ quả không mong muốn
-// var result = 20 / "ne";
-// NaN : Not a Number
-
-// Keyword: JavaScript number methods
-// console.log(isNaN(result));
+1. Tạo mảng
+- Cách tạo
+- Sử dụng cách nào? Tại sao?
+- Kiểm tra data type?
+2. Truy xuất mảng
+ - Độ dài mảng 
+ - Lấy phần tử theo index
+ */
+var languages = ["JavaScrip", "Php", "Ruby"];
