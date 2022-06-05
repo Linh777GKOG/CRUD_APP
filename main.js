8537a08925e26844d50cace6c87a49a2618e524e
Linh Vu <linh777gkog@gmail.com>
2022-06-05 18:03:58 +0700

    defined la xac dinh

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-// Return trong hàm - JavaScript co ban
+/* 
+Một số điều cần biết về function
 
-function cong(a, b) {
-  return a + b;
-  console.log(1234);
+1. Khi function đặt trùng tên: function sau ghi de function truoc
+2. Khai báo biến trong hàm?: Phạm vi trong function 
+3. Định nghĩa hàm trong hàm?: Có
+*/
+
+function showMessage() {
+  function showMessage2() {
+    console.log("Message 2");
+  }
 }
-
-var result = cong(2, 8);
-console.log(result);
+showMessage();
+// defined là định nghĩa
+showMessage2();
