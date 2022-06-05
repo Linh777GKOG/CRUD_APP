9cacc0621693f0d760df7a4bfd8d7266417f570a
Linh Vu <linh777gkog@gmail.com>
2022-06-05 23:00:34 +0700

    lam tron so

--- main.js
+++ main.js
@@ -11,12 +11,14 @@
   - To Fixed
   */
 var age = 18;
-var PI = 3.14;
+var PI = 3000.1235512343;
+console.log(PI.toFixed(2));
 
 // var otherNumber = new Number(9);
 // tranh cach dung new => Vì nó sẽ khởi tạo đối tượng(OBject)
 // đem lại hệ quả không mong muốn
-var result = 20 / "ne";
+// var result = 20 / "ne";
 // NaN : Not a Number
 
-console.log(isNaN(result));
+// Keyword: JavaScript number methods
+// console.log(isNaN(result));
