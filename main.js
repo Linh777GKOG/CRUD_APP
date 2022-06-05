1229455bee35aea0765fba8b54f3a0eadde4a8a6
Linh Vu <linh777gkog@gmail.com>
2022-06-05 22:14:57 +0700

    add 2 toi

--- main.js
+++ main.js
@@ -1,39 +1,5 @@
-// Lam viec voi chuoi
+var million = 1000000;
 
-var myString = "    Hoc Js tai F8!";
+var million = 1e6;
 
-// Keyword: JavaScript string methods
-
-// 1. Length
-// console.log(myString.length);
-
-// 2. Find index
-// console.log(myString.indexOf("Js"));
-/// console.log(myString.indexOf("Js"));
-// console.log(myString.search("Js"));
-
-// 3. Cut String
-// console.log(myString.slice(-3, -1));
-
-// 4. Replace
-// console.log(myString.replace(/Js/g, "JavaScript"));
-
-// 5. Convert  to upper case
-// console.log(myString.toUpperCase());
-
-// 6. Convert to lower case
-// console.log(myString.toLowerCase());
-
-// 7. Trim
-// console.log(myString.trim().length);
-
-// 8. Split
-// var languages = "JavaScript, PhP, Ruby";
-
-// console.log(languages.split(""));
-
-// 9. Get a character by index
-const myString2 = "Linh Vu";
-
-console.log(myString2[10]);
-console.log(myString2.charAt(2));
+var billion = 2e9;
