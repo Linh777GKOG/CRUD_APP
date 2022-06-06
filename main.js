d62c8c5658e782b6d60f74664c4c29d8b114627f
Linh Vu <linh777gkog@gmail.com>
2022-06-06 21:04:06 +0700

    lam viec voi mang

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-// Object trong JavaScript
-var emailKey = "email";
-
-var myInfo = {
-  name: "Linh Vu",
-  // 'name': thừa
-  age: 18,
-  address: "Ha Noi, VN",
-  // đặt tên full-name: vi phạm => "full-name" ko lỗi nữa
-  [emailKey]: "linhvu777gkog@gmail.com",
-  phone: "084502334853",
-  getName: function () {
-    return this.name;
-  },
-  getAge: function () {},
-};
-
-// Function => Phương thức / method
-// Others => Thuộc tính / property
-
-console.log(myInfo.getName());
-
-// myInfo.email = "linhvu777gkog@gmail.com";
-// myInfo["my-email"] = "linhvu777gkog@gmail.com";
-
-// console.log(myInfo.age); c1
-// console.log(myInfo["email"]);
-// một key ko có sẽ trả về undefined
-// console.log(myInfo["address"]); C2
+function joinWithCharacter(array, charactor) {
+  return array.join(`${charactor}`);
+}
