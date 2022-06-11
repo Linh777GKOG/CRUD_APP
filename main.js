690c78bc328549ccb9f6d5e48dba38118d5be651
Linh Vu <linh777gkog@gmail.com>
2022-06-11 23:14:40 +0700

    commit

--- main.js
+++ main.js
@@ -127,8 +127,35 @@ Array methods:
 
 */
 
-function getTotalGold(arr) {
-  return arr.reduce(function (thamso, thamso1) {
-    return thamso + thamso1.gold;
-  }, 0);
-}
+var courses = [
+  {
+    id: 1,
+    name: "JavaScript",
+    coin: 250,
+  },
+  {
+    id: 2,
+    name: "HTML, CSS",
+    coin: 0,
+  },
+  {
+    id: 3,
+    name: "Ruby",
+    coin: 0,
+  },
+  {
+    id: 4,
+    name: "PHP",
+    coin: 400,
+  },
+  {
+    id: 5,
+    name: "ReactJS",
+    coin: 500,
+  },
+];
+
+var totalCoin = courses.reduce(function (total, course) {
+  return total + course.coin;
+}, 0);
+console.log(totalCoin);
