03baef9e8cef2dcc59beb6188ce79b33cd938fbb
Linh Vu <linh777gkog@gmail.com>
2022-06-11 23:33:44 +0700

    commit

--- main.js
+++ main.js
@@ -126,59 +126,16 @@ Array methods:
     reduce()
 
 */
-
-var depthArray = [1, 2, [3, 4], 5, 6, [7, 8, 9]];
-var flatArray = depthArray.reduce(function (flatOutput, depthItem) {
-  return flatOutput.concat(depthItem);
-}, []);
-console.log(flatArray);
-
-var courses = [
-  {
-    id: 1,
-    name: "JavaScript",
-    coin: 250,
-  },
-  {
-    id: 2,
-    name: "HTML, CSS",
-    coin: 0,
-  },
-  {
-    id: 3,
-    name: "Ruby",
-    coin: 0,
-  },
-  {
-    id: 4,
-    name: "PHP",
-    coin: 400,
-  },
-  {
-    id: 5,
-    name: "ReactJS",
-    coin: 500,
-  },
-];
-
-var numbers = [100, 200, 300, 500, 600];
-
-var totalCoin = numbers.reduce(function (total, number) {
-  return total + number;
-}, 0);
-
-console.log(totalCoin);
-
-var newCourses = topics.reduce(function (courses, topic) {
-  return courses.concat(topic.courses);
-}, []);
-console.log(newCourses);
-
-var html = newCourses.map(function (course) {
-  return `<div>
-    <h2>${course.title}</h2>
-    <p>ID: ${course.id}</p>
-    
-    </div>`;
-});
-console.log(htmls);
+function calculateRating(watchList) {
+  const newArray = watchList.filter(
+    (array) => array["Director"] === "Christopher Nolan"
+  );
+  const scoreIMDB = newArray.reduce((total, item, index) => {
+    total += Number(item.imdbRating);
+    if (index === newArray.length - 1) {
+      return total / newArray.length;
+    }
+    return total;
+  }, 0);
+  return scoreIMDB;
+}
