c022889c60a6f3c7d0a12ee0530cbda569ac1693
Linh Vu <linh777gkog@gmail.com>
2022-06-12 21:49:10 +0700

    reduce2

--- main.js
+++ main.js
@@ -327,10 +327,16 @@ Array methods:
 // trong hàm(function ) map() khi ko có đối số => undefined(),
 // gọi cái hư vô nên hiểu là undefined,
 // cần truyền 1 function vào trong method map() => Ko Bug
+Array.prototype.reduce2 = function (callback, result) {
+  for (let i = 0; i < this.length; i++) {
+    result = callback(result, this[i], i, this);
+  }
+  return result;
+};
 
 const numbers = [1, 2, 3, 4, 5];
 
-const result = numbers.reduce((total, number) => {
+const result = numbers.reduce2((total, number) => {
   return total + number;
-}, 10);
+}, 0);
 console.log(result);
