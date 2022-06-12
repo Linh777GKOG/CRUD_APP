cc22ac1b3a4f2358d62c3706fd1932589b33cc09
Linh Vu <linh777gkog@gmail.com>
2022-06-12 23:05:16 +0700

    commit

--- main.js
+++ main.js
@@ -364,11 +364,27 @@ Array methods:
 // 1. La ham
 // 2. Truyen qua doi so
 // 3. Duoc goi lai (trong ham nhan doi so)
+Array.prototype.map2 = function (callback) {
+  var output = [],
+    arrayLength = this.length;
+  for (var i = 0; i < this.length; i++) {
+    var result = callback(this[i], i);
+    output.push(result);
+  }
+  return output;
+};
 
 var courses = ['JavaScript', 'PHP', 'Ruby'];
-var htmls = courses.map(function (course) {
+
+var htmls = courses.map2(function (course) {
   return `<h2>
   ${course}</h2>`;
 });
-
 console.log(htmls.join(''));
+
+// var htmls = courses.map2(function (course) {
+//   return `<h2>
+//   ${course}</h2>`;
+// });
+
+// console.log(htmls.join(''));
