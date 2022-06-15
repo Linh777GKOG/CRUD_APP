26f85928a0804838aa612e675c239f38abc16ce8
Linh Vu <linh777gkog@gmail.com>
2022-06-15 18:49:25 +0700

    Commit

--- main.js
+++ main.js
@@ -423,17 +423,23 @@ for (i = 0; i < arr.length; i++) {
 
 // f(n) = cn O(f(n)) = O(n)
 
-var lengthOfLongestSubstring = function (s) {
-  let max = 0;
-  let begin = 0;
-  let map = {};
+function medianOf2Arr(arr1, arr2) {
+  var concat = arr1.concat(arr2);
+  concat = concat.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(concat);
+  var length = concat.length;
 
-  for (let end = 0; end < s.length; end++) {
-    if (map[s[end]] !== undefined && map[s[end]] >= begin) {
-      begin = map[s[end]] + 1;
-    }
-    map[s[end]] = end;
-    max = Math.max(max, end - begin + 1);
+  if (length % 2 == 1) {
+    //length is odd
+    console.log(concat[length / 2 - 0.5]);
+    return concat[length / 2 - 0.5];
+  } else {
+    console.log((concat[length / 2] + concat[length / 2 - 1]) / 2);
+    return (concat[length / 2] + concat[length / 2 - 1]) / 2;
   }
-  return max;
-};
+}
+arr1 = [1, 4, 7, 9];
+arr2 = [2, 5, 8];
+medianOf2Arr(arr1, arr2);
