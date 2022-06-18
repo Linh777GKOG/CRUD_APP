9174bbc1be22fc3bbae80cc60580218d5400d10a
Linh Vu <linh777gkog@gmail.com>
2022-06-18 20:42:31 +0700

    commit

--- main.js
+++ main.js
@@ -392,4 +392,12 @@ Array methods:
 // 2. For in
 // 3. hasOwnProperty
 
-console.log(document.anchors);
+// 1. getElementById
+// 2. getElementsByClassName
+// 3. getElementsByTagName
+// 4. querySelector
+// 5. querySelectorAll
+
+// 6. HTML collection: img, form, a
+
+// 7. document.write
