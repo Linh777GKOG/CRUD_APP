21926f4569b0b8425b698b1db09c93019c08ff29
Linh Vu <linh777gkog@gmail.com>
2022-06-25 21:42:25 +0700

    nhan nhieu ham voi nhau den vo han

--- main.js
+++ main.js
@@ -767,3 +767,35 @@ function multiply() {
 multiply(); // Returns 60
 
 // Một ví dụ hàm lồng nhau
+function getScore () {
+  var num = 50,
+  num2 = 50,
+
+  function add() {
+    return name + " scored " + (num1 + num2);
+  }
+
+  return add();
+}
+
+function multiply(a,b) {
+  b = typeof b !== 'undefined' ? b : 1;
+
+  return a * b;
+}
+
+multiply(5); // 5
+
+function multiply(a, b = 1 ) {
+  return a * b;
+}
+
+multiply(5);
+
+function multiply(multiplier, ...theArgs) {
+  return theArgs.map(x => multiplier * x);
+}
+
+var arr = multiply(2,1,2,3);
+  console.log(arr); // [2, 4, 6]
+
