ba2100ea0bee076af7c1e0e9dbf46b6e23888d51
Linh Vu <linh777gkog@gmail.com>
2022-06-25 21:45:45 +0700

    ham beo

--- main.js
+++ main.js
@@ -799,3 +799,15 @@ function multiply(multiplier, ...theArgs) {
 var arr = multiply(2,1,2,3);
   console.log(arr); // [2, 4, 6]
 
+// (param1, param2, paramN) => expression
+
+// ES5
+var multiply = function(x, y) {
+  return x * y;
+}
+
+// ES6
+var multiply = (x, y) => {
+  return x * y;
+}
+// => var multiply = (x, y) => x * y;
