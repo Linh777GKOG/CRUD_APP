a7769070f5bbd5da53a87707f5bce001436cd3d8
Linh Vu <linh777gkog@gmail.com>
2022-06-25 22:33:35 +0700

    kiem tra fetch

--- main.js
+++ main.js
@@ -910,3 +910,11 @@ var personInfo = {
 
 }
 
+if (!('fetch' in window)) {
+  console.log('Fetch API not found, try including the polyfill');
+  return ;
+}
+
+// We can safely use fetch from now on
+
+//
