55799fa6260efe9f09740ad8fbbd6ed9c07643b6
Linh Vu <linh777gkog@gmail.com>
2022-06-25 22:00:07 +0700

    ham noi bo ket thuc

--- main.js
+++ main.js
@@ -835,3 +835,32 @@ var hello = () => {
 }
 
 hello();
+
+function numberGenerator() {
+  // Local "free" variable that ends within the closure
+  var num = 1 ;
+  function checkNumber() {
+    console.log(num);
+  }
+  num++;
+
+  return checkNumber;
+}
+
+var number = numberGenerator();
+number(); //2
+
+function sayHello() {
+  var say = function() {
+    console.log(hello);
+  }
+  // Local variable that ends up within the closure
+  var hello = 'Hello, world';
+
+  return say;
+
+}
+
+var sayHelloClosure = sayHello();
+sayHelloClosure(); // 'Hello, world'
+
