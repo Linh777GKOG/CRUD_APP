9d991d27cd8dc59e2a0ed58c37b1dcfbdb8bf207
Linh Vu <linh777gkog@gmail.com>
2022-07-08 16:29:02 +0700

    push code

--- main.js
+++ main.js
@@ -138,7 +138,9 @@ Array methods:
     reduce(): khi muốn nhận về 1 giá trị duy nhất sau khi tính toán xử lỉ phần
 // tử trong array, 
 
+
 */
+//* This is highlighted*/
 
 // Callback?
 
@@ -202,7 +204,7 @@ Array methods:
 // 10. Mistakes
 // 11. Performance
 
-var coursesAPI = 'http://localhost:3000/courses';
+var courseApi = 'http://localhost:3000/courses';
 
 function start() {
   getCourses(renderCourses);
@@ -214,28 +216,76 @@ start();
 
 // Functions
 function getCourses(callback) {
-  fetch(coursesAPI)
+  fetch(courseApi)
     .then(function (response) {
       return response.json();
     })
     .then(callback);
 }
 
+function createCourse(data, callback) {
+  var options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+
+    body: JSON.stringify(data),
+  };
+  fetch(courseApi, options)
+    .then(function (response) {
+      response.json();
+    })
+    .then(callback);
+}
+
+function handleDeleteCourse(id) {
+  var options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  fetch(courseApi + '/' + id, options)
+    .then(function (response) {
+      response.json();
+    })
+    .then(function () {
+      var courseItem = document.querySelector('.course-item-' + id);
+      if (courseItem) {
+        courseItem.remove();
+      }
+    });
+}
+
 function renderCourses(courses) {
   var listCoursesBlock = document.querySelector('#list-courses');
   var htmls = courses.map(function (course) {
-    return `<li>
-   <h4>${course.name}</h4> 
-   <p>${course.description}</p> 
-    </li>`;
+    return `
+   <li class="course-item-${course.id}">
+   <h4>${course.name}</h4>
+   <p>${course.description}</p>
+   <button onclick="handleDeleteCourse(${course.id})">Xoa</button>
+   </li> 
+    `;
   });
   listCoursesBlock.innerHTML = htmls.join('');
 }
 
 function handleCreateForm() {
   var createBtn = document.querySelector('#create');
+
   createBtn.onclick = function () {
     var name = document.querySelector('input[name="name"]').value;
-    console.log(name);
+    var description = document.querySelector('input[name="description"]').value;
+
+    var formData = {
+      name: name,
+      description: description,
+    };
+
+    createCourse(formData, function () {
+      getCourses(renderCourses);
+    });
   };
 }
